4a4d6d2a68aab13c2eff91b81ed43f49700ab313
Mike Grant <mgrant@hc3.io>
2022-05-16 20:22:35 -0500

    Add parameterized statement generation.

--- FileProcessingPerfTesting.Cli/Program.cs
+++ FileProcessingPerfTesting.Cli/Program.cs
@@ -1,10 +1,40 @@
-﻿// Establish processing folder.
-using FileProcessingPerfTesting.DataGenerator;
+﻿using FileProcessingPerfTesting.DataGenerator;
 
+// Establish processing folder.
 string processingFolderName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "FileProcessingPerfTesting");
 Directory.CreateDirectory(processingFolderName);
 
 var processingDirectory = new DirectoryInfo(processingFolderName);
+int fileCount = 4;
+int statementCount = 1000;
 
-string testFile = Path.Combine(processingDirectory.FullName, "text.xml");
-StatementGenerator.SaveStatementsToXml(testFile, 1000, 100_000);
+IEnumerable<FileInfo> filelist = GetExistingProcessingFiles(processingDirectory);
+//IEnumerable<FileInfo> filelist = GetNewProcessingFiles(processingDirectory, fileCount, statementCount);
+
+foreach (var file in filelist)
+{
+    Console.WriteLine($"Processing {file.Name}");
+}
+
+
+IEnumerable<FileInfo> GetNewProcessingFiles(DirectoryInfo processingDirectory, int fileCount, int statementCount)
+{
+    processingDirectory
+        .EnumerateFiles("*.xml")
+        .ToList()
+        .ForEach(file => file.Delete());
+
+    for(int i=1; i<= fileCount; i++)
+    {
+        string filename = Path.Combine(processingDirectory.FullName, $"statement{i}.xml");
+        StatementGenerator.SaveStatementsToXml(filename, statementCount, i * 100_000);
+    }
+
+    return GetExistingProcessingFiles(processingDirectory);
+}
+
+IEnumerable<FileInfo> GetExistingProcessingFiles(DirectoryInfo processingDirectory)
+{
+    return processingDirectory.EnumerateFiles("*.xml");
+}
+
